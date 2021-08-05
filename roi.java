54494689cb5ae22e9951fcbfd43f2a1571b3408e
alexander-krug <62189212+alexander-krug@users.noreply.github.com>
2021-08-05 19:40:17 +0200

    Create roi.java

---
+++ roi.java
@@ -0,0 +1,8 @@
+public class roi
+  {
+    public static void main (String args[])
+    { float p, r, t, si; // principal amount, rate, time and simple interest respectively
+        p =50000; r = 12; t = 30;
+          si = (p*r*t)/100;
+          System.out.println("Simple interest is: " +si);
+    }}
